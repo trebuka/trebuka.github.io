1c7bdd731072ecfccb66db400d3e821abd819bc3
trebuka <52698807+trebuka@users.noreply.github.com>
2024-04-07 11:14:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -322,7 +322,11 @@ function completedDiv() {
 
   for (let i = 0; i <= taskNumber; i++) {
       if (tasksArray[i] && tasksArray[i].checked == "checked") {
-          allTask.insertAdjacentHTML('afterBegin', htmlAdd(tasksArray[i].id, tasksArray[i].title, tasksArray[i].description, tasksArray[i].date, tasksArray[i].time, tasksArray[i].status, tasksArray[i].checked));
+          allTask.insertAdjacentHTML('afterBegin', htmlAdd(tasksArray[i].id, tasksArray[i].title, tasksArray[i].description, tasksArray[i].date, tasksArray[i].time, tasksArray[i].status, tasksArray[i].checked, tasksArray[i].completedTime));
+          if(tasksArray[i].completedTime == null){
+      console.log(tasksArray[i].completedTime)
+      document.getElementById(`completedTime_${i+1}`).style.display = "none";
+    }
       }
   }
 }
